a0babeae67053b8b9ef2dd2aa394b03f09e6aa31
Maris-Pidiks <maris@divikiwi.lv>
2025-07-14 10:20:08 +0300

    db schema and functions are completed

---
+++ convex/todo.ts
@@ -0,0 +1,78 @@
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "../convex/_generated/server";
+
+//iegūstam todos no db
+
+export const getTodos = query({
+    handler: async (ctx) => {
+        const todos = await ctx.db.query("todos").order("desc").collect();
+        return todos;
+    }
+});
+
+// todo pievienoshanas funkcija
+
+export const addTodo = mutation({
+    args: { text: v.string() },
+    handler: async (ctx, args) => {
+        const todoId = await ctx.db.insert("todos", {
+            text: args.text,
+            isCompleted: false
+        });
+        return todoId;
+    }
+});
+
+
+// pārslēgs pogai - ir vai nav todo
+
+export const toggleTodo = mutation({
+    args: { id: v.id("todos") },
+    handler: async (ctx, args) => {
+        const todo = await ctx.db.get(args.id);
+        if (!todo) {
+            throw new ConvexError("Todo not found");
+        }
+        await ctx.db.patch(args.id, {
+            isCompleted: !todo.isCompleted
+        });
+    }
+})
+
+// todo dzēšanas funkcija
+
+export const deleteTodo = mutation({
+    args: { id: v.id("todos") },
+    handler: async (ctx, args) => {
+        await ctx.db.delete(args.id);
+    }
+})
+
+// todo rediģēšanas funkcija
+
+export const updateTodo = mutation({
+    args: { id: v.id("todos"), text: v.string() },
+    handler: async (ctx, args) => {
+        await ctx.db.patch(args.id, {
+            text: args.text
+        });
+    }
+})
+
+// reset app btn funkcija (visi todos tiek dzēsti)
+
+export const clearAllTodos = mutation({
+    handler: async (ctx) => {
+        const todos = await ctx.db.query("todos").collect();
+
+
+        //Delete all todos
+        for (const todo of todos) {
+            await ctx.db.delete(todo._id);
+        }
+
+        return { deleteCount: todos.length };
+    }
+})
+
+
